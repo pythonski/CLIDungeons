44d5f04fb9cfecdc838f84c97593601c21305977
Pythonete <fabio.marinello@protonmail.com>
2023-09-03 00:20:59 +0200

    added fight function to encounter and fixed character class

--- character.py
+++ character.py
@@ -2,18 +2,20 @@ from random import randint
 
 class Character:
     
-    def __init__(self, isPlayer, name, role, attack, armor, health, magic, agility, inventory, abilities):
+    def __init__(self, isPlayer, name, role, attack, armor, health, magic, agility, money, inventory, abilities):
         self.isPlayer = isPlayer
         self.name = name
         self.role = role
         self.attack = attack
+        self.armor = armor
         self.health = health
         self.magic = magic
         self.agility = agility
+        self.money = money
         self.inventory = inventory
         self.abilities = abilities
         
-    def throw():
+    def throw(self):
         result = []
         for die in range(self.attack):
             result.append(randint(1,6))
